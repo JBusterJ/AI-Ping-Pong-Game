16865dc7a7b1d825ad9d4e2ac05b9581b484a769
JBusterJ <50986806+JBusterJ@users.noreply.github.com>
2022-03-09 17:44:46 +1100

    sound affects

--- main.js
+++ main.js
@@ -36,6 +36,8 @@ function setup() {
   vid = createCapture(VIDEO);
   vid.size(700, 600);
   vid.hide();
+  hitSound = loadSound("ball_touch_paddel.wav");
+  missSound = loadSound("missed.wav")
   var poseNet = ml5.poseNet(vid, modelLoaded);
   poseNet.on('pose', gotPoses);
 }
@@ -77,8 +79,8 @@ function draw() {
     fill(250, 0, 0);
     stroke(0, 0, 250);
     strokeWeight(0.5);
-    // paddle1Y = rightWristY;
-    paddle1Y = mouseY;
+    paddle1Y = rightWristY;
+    // paddle1Y = mouseY;
     rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
 
 
@@ -163,9 +165,11 @@ function move() {
   if (ball.x - 2.5 * ball.r / 2 < 0) {
     if (ball.y >= paddle1Y && ball.y <= paddle1Y + paddle1Height) {
       ball.dx = -ball.dx + 0.5;
+      hitSound.play()
     }
     else {
       pcscore++;
+      missSound.play();
       reset();
       navigator.vibrate(100);
     }
@@ -178,7 +182,7 @@ function move() {
     stroke("white");
     textSize(25)
     text("Game Over!☹☹", width / 2, height / 2);
-    text("Reload The Page!", width / 2, height / 2 + 30)
+    text("Press the restart button to try again!", width / 2, height / 2 + 30)
     noLoop();
     pcscore = 0;
   }
